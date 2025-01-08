15f15d3c16eafa8c690a1501bd470a2430aad4c2
Jackson Romero <jacksontromero@gmail.com>
2025-01-07 20:00:37 -0500

    router refresh after published class

--- src/components/sidebar/ClassOptions.tsx
+++ src/components/sidebar/ClassOptions.tsx
@@ -137,6 +137,7 @@ export default function ClassOptions(params: {
 
     form.reset();
     setOpen(false);
+    router.refresh();
   };
 
   const deleteClass = useDataStore((state) => state.deleteClass);
